7c27a90b227bc80f519e54f3513bc61430de775e
infravisional <yoojh12@gmail.com>
2021-12-29 14:57:19 -0800

    Update 2_Sample_Hudi_Work.py

--- Hudi/2_Sample_Hudi_Work.py
+++ Hudi/2_Sample_Hudi_Work.py
@@ -1,1 +1,32 @@
+# Create a DataFrame
+inputDF = spark.createDataFrame(
+    [
+        ("100", "2015-01-01", "2015-01-01T13:51:39.340396Z"),
+        ("101", "2015-01-01", "2015-01-01T12:14:58.597216Z"),
+        ("102", "2015-01-01", "2015-01-01T13:51:40.417052Z"),
+        ("103", "2015-01-01", "2015-01-01T13:51:40.519832Z"),
+        ("104", "2015-01-02", "2015-01-01T12:15:00.512679Z"),
+        ("105", "2015-01-02", "2015-01-01T13:51:42.248818Z"),
+    ],
+    ["id", "creation_date", "last_update_time"]
+)
 
+# Specify common DataSourceWriteOptions in the single hudiOptions variable
+hudiOptions = {
+'hoodie.table.name': 'my_hudi_table',
+'hoodie.datasource.write.recordkey.field': 'id',
+'hoodie.datasource.write.partitionpath.field': 'creation_date',
+'hoodie.datasource.write.precombine.field': 'last_update_time',
+'hoodie.datasource.hive_sync.enable': 'true',
+'hoodie.datasource.hive_sync.table': 'my_hudi_table',
+'hoodie.datasource.hive_sync.partition_fields': 'creation_date',
+'hoodie.datasource.hive_sync.partition_extractor_class': 'org.apache.hudi.hive.MultiPartKeysValueExtractor'
+}
+
+# Write a DataFrame as a Hudi dataset
+inputDF.write
+.format('org.apache.hudi')
+.option('hoodie.datasource.write.operation', 'insert')
+.options(**hudiOptions)
+.mode('overwrite')
+.save('s3://DOC-EXAMPLE-BUCKET/myhudidataset/')
